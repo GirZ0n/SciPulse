414f55b6485eda8ba27f830b43c2ddfa29376f23
Ilya Vlasov <ilya.vlasov@jetbrains.com>
2024-08-30 13:38:41 +0400

    feat: init bot/post-feeds

---
+++ packages/bot/post_papers/__main__.py
@@ -0,0 +1,14 @@
+import os
+
+from feedgen.feed import FeedGenerator
+import feedparser
+
+from utils import copy_channel_metadata, convert_item, check_secret_key
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
