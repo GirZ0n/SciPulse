2828f69ffb6ffc1c006fd26cf1efd663ef37e640
Ilya Vlasov <ilya.vlasov@jetbrains.com>
2024-09-04 14:04:09 +0400

    fix: replace logger with logging

--- packages/bot/handle_interactions/__main__.py
+++ packages/bot/handle_interactions/__main__.py
@@ -8,8 +8,6 @@ from paper_post import PaperPost
 from wrappers import update_message
 from security_utils import check_secret_key
 
-logger = logging.getLogger(__name__)
-
 
 def main(event, context):
     if not check_secret_key(event):
@@ -18,33 +16,33 @@ def main(event, context):
     slack_token = os.environ["SLACK_BOT_TOKEN"]
     client = WebClient(token=slack_token)
 
-    logger.info('Receiving interaction action')
+    logging.info('Receiving interaction action')
 
-    payload = json.loads(event['payload'])
-    logger.debug(f'Payload: {event["payload"]}')
+    payload = json.loads(event['payload'][0])
+    logging.debug(f'Payload: {event["payload"][0]}')
 
     channel = payload['container']['channel_id']
-    logger.debug(f'Channel: {channel}')
+    logging.debug(f'Channel: {channel}')
 
     ts = payload['container']['message_ts']
-    logger.debug(f'TS: {ts}')
+    logging.debug(f'TS: {ts}')
 
     metadata = payload['message']['metadata']
-    logger.debug(f'Metadata: {metadata}')
+    logging.debug(f'Metadata: {metadata}')
 
     action = payload['actions'][0]['action_id']
-    logger.debug(f'Action: {action}')
+    logging.debug(f'Action: {action}')
 
     username = payload['user']['username']
-    logger.debug(f'Username: {username}')
+    logging.debug(f'Username: {username}')
 
-    logger.info(f'Reading post')
+    logging.info(f'Reading post')
     paper_post = PaperPost.from_slack_metadata(metadata)
 
-    logger.info('Updating state')
+    logging.info('Updating state')
     paper_post.update_state(action, username)
 
-    logger.info('Updating posts')
+    logging.info('Updating posts')
     update_message(
         client,
         channel=channel,
