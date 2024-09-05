0d62863a35188ed41d76511ba35ff33ee98df90b
Ilya Vlasov <ilya.vlasov@jetbrains.com>
2024-09-05 13:57:00 +0400

    feat: add checking slack secret

--- lib/security_utils.py
+++ lib/security_utils.py
@@ -1,6 +1,22 @@
+import json
 import os
 import secrets
-from typing import Dict
+from typing import Dict, Optional
+
+from slack_sdk.signature import SignatureVerifier
+from urllib.parse import parse_qs
+
+
+def handle_slack_request(event: Dict) -> Optional[Dict]:
+    verifier = SignatureVerifier(os.environ["SLACK_SIGNING_SECRET"])
+
+    body = event['http']['body']
+    headers = event['http']['headers']
+
+    if not verifier.is_valid_request(body, headers):
+        return None
+
+    return json.loads(parse_qs(body)['payload'][0])
 
 
 def check_secret_key(event: Dict) -> bool:
